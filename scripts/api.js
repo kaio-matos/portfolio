36d8f755608b5258921fd0d3ae4a72d47dd8a49b
Kaio <kaio_matos22@hotmail.com>
2021-11-03 11:05:29 -0300

    Simple Github API integration

---
+++ scripts/api.js
@@ -0,0 +1,22 @@
+/**
+ * Class that provides functions for github API requests
+ */
+class GithubAPI {
+    username = "kaio-matos"
+    api = "https://api.github.com"
+
+    async getRepos(){
+        const raw = await fetch(`${this.api}/users/${this.username}/repos`, {
+            method: "GET"
+        })
+
+        const repos = await raw.json()
+        return repos
+    }
+
+}
+
+
+const ghApi = new GithubAPI()
+
+export { ghApi }
---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+import { ghApi } from "./api.js";
+
+async function getRepositories() {
+    const repos = await ghApi.getRepos()
+    console.log(repos)
+}
+
+(() => {
+    getRepositories()
+})()
