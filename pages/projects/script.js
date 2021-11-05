8b0c0d1bb45afd5785f4e417c1fef0efbec8d571
Kaio <kaio_matos22@hotmail.com>
2021-11-05 17:30:38 -0300

    [Projects] Adding repositories cards styles, fetch code and render code

--- pages/projects/script.js
+++ pages/projects/script.js
@@ -7,14 +7,58 @@ import { ghApi } from "../../scripts/api.js";
 })();
 
 function createCards(repos, element) {
-  // repos.forEach((repo) => {
-  //   const card = Card(repo);
-  //   element.appendChild(card);
-  // });
+  repos.forEach((repo) => {
+    const card = Card(repo);
+    element.appendChild(card);
+  });
 }
+
+{
+  /* <div class="card">
+  <div class="card_image_container">
+    <img src="https://source.unsplash.com/random" alt="" />
+  </div>
+  <div class="card_info">
+    <h2 class="card_title title text_gray">churrascometro</h2>
+    <div class="card_description"></div>
+  </div>
+</div>; */
+}
+
 function Card({ name, description, topics, homepage, html_url, language }) {
-  const div = document.createElement("div");
-  const txt = document.createTextNode(name);
-  div.appendChild(txt);
-  return div;
+  // Containers
+  const card = document.createElement("div");
+  const cardImageContainer = document.createElement("div");
+  const cardInfo = document.createElement("div");
+
+  card.classList.add("card");
+  cardImageContainer.classList.add("card_image_container");
+  cardInfo.classList.add("card_info");
+
+  // Image
+  const img = document.createElement("img");
+  img.src = "https://source.unsplash.com/random"; // For now
+  img.alt = name;
+
+  cardImageContainer.appendChild(img);
+
+  // Infos
+  const h2 = document.createElement("h2");
+  const title = document.createTextNode(name);
+
+  h2.appendChild(title);
+  h2.classList.add("card_title", "title", "text_gray");
+
+  const descriptionDiv = document.createElement("div");
+  const descText = document.createTextNode(description);
+
+  descriptionDiv.appendChild(descText);
+  descriptionDiv.classList.add("card_description", "text_gray");
+
+  cardInfo.appendChild(h2);
+  cardInfo.appendChild(descriptionDiv);
+
+  card.appendChild(cardImageContainer);
+  card.appendChild(cardInfo);
+  return card;
 }
